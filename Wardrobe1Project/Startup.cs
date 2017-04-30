6502dd1cea592a69a9ad7efe4d13d4c0db41da04
mlane0399 <lane.293@gmail.com>
2017-04-30 11:35:30 -0400

    first commit

---
+++ Wardrobe1Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wardrobe1Project.Startup))]
+namespace Wardrobe1Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
